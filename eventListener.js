b8ee2de530edfb656d87b9afc1672f3a04c2935a
Lazit <31299536+Lazit@users.noreply.github.com>
2022-04-12 17:46:50 +0900

    Update eventListener.js

--- eventListener.js
+++ eventListener.js
@@ -1,20 +1,44 @@
-const title = document.querySelector(".yeah")
+const h1 = document.querySelector(".yeah")
 
-function handleTitleClick() {
-    console.log("title was clicked!");
-    title.style.color = "blue";
+function handleh1Click() {
+    console.log("This was clicked!");
+    h1.style.color = "blue";
 }
 //개발자가 직접 ()로 함수 실행하지 않고 이벤트에 넘겨주어 이벤트가 실행되면 함수 실행되도록 함.
 
 function hnadleMouseEnter() {
-    title.innerText = "Mouse is here!"
+    h1.innerText = "Mouse is here!"
 }
 
 
 function handleMouseLeave() {
-    title.innerText = "Mouse is gone!"
+    h1.innerText = "Mouse is gone!"
 }
 
-title.addEventListener("mouseenter", hnadleMouseEnter);
-title.addEventListener("mouseleave", handleMouseLeave);
-title.addEventListener("click", handleTitleClick);
+function handleWindowResize() {
+    document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy() {
+    alert("copier!");
+}
+function handleWindowOffline() {
+    alert("SOS no Wifi");
+}
+
+function handleWindowOnline() {
+    alert("ALL GOOOOD")
+}
+
+h1.addEventListener("mouseenter", hnadleMouseEnter);
+h1.addEventListener("mouseleave", handleMouseLeave);
+h1.addEventListener("click", handleh1Click);
+// =-> title.onclick = handleTitleClick;
+
+
+//addeventlistener 는 길긴하지만, 나중에 removeeventlestener 로 제어가능.
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("offline", handleWindowOnline);
