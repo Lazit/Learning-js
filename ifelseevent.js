89104335804732323ff3814d1a05406f6b247918
Lazit <31299536+Lazit@users.noreply.github.com>
2022-04-12 17:54:29 +0900

    event update

---
+++ ifelseevent.js
@@ -0,0 +1,14 @@
+const h2 = document.querySelector(".yeahyeah")
+
+function handleh2Click() {
+    const currentColor = h2.style.color;
+    let newColor;
+    if(currentColor === "red") {
+      newColor = "blue"  
+    } else {
+        newColor = "red";
+    }
+    h2.style.color = newColor;
+}
+
+h2.addEventListener("click", handleh2Click)
